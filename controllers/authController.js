2669ea06b0f9ce8f6cab469bbe0e94bc1e9b0931
Minh Tam <caominhtam.10a4@gmail.com>
2023-08-15 22:07:26 +0700

    feat/auth-controller: add login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -23,17 +25,29 @@ const createSendToken = (user, statusCode, req, res) => {
   });
 };
 
-exports.signup = async (req, res) => {
+exports.signup = catchAsync(async (req, res) => {
   const { name, email, password, passwordConfirm } = req.body;
 
   const newUser = await User.create({ name, email, password, passwordConfirm });
 
   createSendToken(newUser, 201, req, res);
-};
+});
 
-exports.login = async (req, res) => {
-  res.status(200).json({ status: 'success' });
-};
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password.', 401));
+  }
+
+  createSendToken(user, 200, req, res);
+});
 
 exports.logout = async (req, res) => {
   res.status(200).json({ status: 'success' });
