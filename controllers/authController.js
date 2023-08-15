6d909f744145a71958d15acf77aed01a533ce0a3
Minh Tam <caominhtam.10a4@gmail.com>
2023-08-15 21:51:13 +0700

    feat/auth-controller: add signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,48 @@
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/userModel');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
+const createSendToken = (user, statusCode, req, res) => {
+  const token = signToken(user._id);
+
+  user.password = undefined;
+  user.active = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+};
+
 exports.signup = async (req, res) => {
+  const { name, email, password, passwordConfirm } = req.body;
+
+  const newUser = await User.create({ name, email, password, passwordConfirm });
+
+  createSendToken(newUser, 201, req, res);
+};
+
+exports.login = async (req, res) => {
+  res.status(200).json({ status: 'success' });
+};
+
+exports.logout = async (req, res) => {
+  res.status(200).json({ status: 'success' });
+};
+
+exports.forgotPassword = async (req, res) => {
+  res.status(200).json({ status: 'success' });
+};
+
+exports.resetPassword = async (req, res) => {
   res.status(200).json({ status: 'success' });
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,9 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
