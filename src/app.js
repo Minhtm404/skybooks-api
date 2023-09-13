f0bbe72b25ec30c26780dd824d6f11af268f3f90
Minh Tam <caominhtam.10a4@gmail.com>
2023-09-14 00:04:15 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 app.use('/api/v1/collections', collectionRouter);
