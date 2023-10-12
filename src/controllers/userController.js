59039cb603ec1aa2488a6713b3c8d517af8c39d8
Minh Tam <caominhtam.10a4@gmail.com>
2023-10-13 00:49:10 +0700

    feat/admin: change  get all

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,7 +21,7 @@ exports.query = catchAsync(async (req, res, next) => {
 });
 
 exports.queryAdmin = catchAsync(async (req, res, next) => {
-  if (!req.query.role === 'admin' || !req.query.role === 'staff') {
+  if (!req.query.role === 'admin' && !req.query.role === 'staff') {
     req.query.role = ['admin', 'staff'];
   }
 
