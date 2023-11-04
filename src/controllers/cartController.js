9446bbb7621230fda81b8b4132ec51cac77cf9be
Minh Tam <caominhtam.10a4@gmail.com>
2023-11-04 23:14:53 +0700

    remove unused

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,6 +1,5 @@
 const factory = require('./handlerFactory');
 const CartItem = require('../models/cartItemModel');
-const catchAsync = require('../utils/catchAsync');
 
 exports.getAllCartItems = factory.getAll(CartItem);
 exports.createCartItem = factory.createOne(CartItem);
